cb5147b14933c72dd40372c11cff3ac94eac8a46
samusevich yuriy <wrathofthelichking777@gmail.com>
2019-10-26 19:37:02 +0500

    [FIX] moved secret key to .env

--- django-app/django_shop/django_app/settings.py
+++ django-app/django_shop/django_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oic6!r$^m2!%w$kb8khmxf^1uf2--z4e9*z6($o0&uc5@d0ni#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

2d259430810c4146f2e9961b1b0d67e6440a7798
samusevich yuriy <wrathofthelichking777@gmail.com>
2019-10-28 21:22:55 +0500

    [FEAT] added small img for preview in admin panel

--- django-app/django_shop/socks_shop/admin.py
+++ django-app/django_shop/socks_shop/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from .models import Socks, Stock
+from django.utils.safestring import mark_safe
 
 
 class AdminSocks(admin.ModelAdmin):
-    list_display = ['available', 'price', 'color', 'slug', 'name']
+    readonly_fields = ['image_tag']
+
+    list_display = ['available', 'price', 'color', 'slug', 'name', 'image', 'image_tag']
     list_filter = ['available', 'price', 'color']
-    list_editable = ['price', 'color']
+    list_editable = ['price', 'color', 'image']
 
     prepopulated_fields = {'slug': ('name', )}
 

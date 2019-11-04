203877b844d8cc1907a4eed851e307f7adc5df18
samusevich yuriy <wrathofthelichking777@gmail.com>
2019-11-04 16:33:53 +0500

    [FEAT] change some desing

--- django-app/django_shop/socks_shop/views.py
+++ django-app/django_shop/socks_shop/views.py
@@ -1,10 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Socks
 
 
 def return_items(request):
-    items = Socks.objects.all()
+    items = Socks.objects.filter(available=True)
 
     return render(request=request, template_name='socks_shop/index.html', context={'items': items})
 

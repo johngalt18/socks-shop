ccce400a1ef88c780bad3811a8bb8cdf6c80254d
samusevich yuriy <wrathofthelichking777@gmail.com>
2019-11-11 16:56:01 +0500

    [FIX] added base render for socks sizes

--- django-app/django_shop/socks_shop/views.py
+++ django-app/django_shop/socks_shop/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Socks, Stock
+from .forms import StockForm
+from django.views.generic.edit import CreateView
+
+from django.views.decorators.http import require_http_methods
 
 
 def return_items(request):
     items = Socks.objects.filter(available=True)
 
     return render(request=request, template_name='socks_shop/index.html', context={'items': items})
+
+
+class CreateMyModelView(CreateView):
+    model = Stock
+    form_class = StockForm
+    template_name = 'socks_shop/template.html'
+    success_url = 'socks_shop/success.html'
 
 
 def item_detail(request, slug):
